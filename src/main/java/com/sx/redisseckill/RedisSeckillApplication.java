824d622addb2ac76694a70853d0459ea033be1a7
孙旭 <34908553+sunxu654@users.noreply.github.com>
2019-07-11 09:33:16 +0800

    项目构建完成

---
+++ src/main/java/com/sx/redisseckill/RedisSeckillApplication.java
@@ -0,0 +1,13 @@
+package com.sx.redisseckill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisSeckillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisSeckillApplication.class, args);
+    }
+
+}
